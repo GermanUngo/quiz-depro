5cc5fa775524cb749b719128d113d735dd2b2893
GERMAN IGNACIO UNGO CORRALES <germanungo@gmail.com>
2023-03-02 23:28:10 -0300

    Configuração inicial/gitignore/FrontEnd/Mapeamento de url

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(requisicao):
+    return render(requisicao, 'base/home.html')
+
+
+def perguntas(requisicao, indice):
+    contexto = {'indice_da_questao':indice}
+    return render(requisicao, 'base/game.html', context=contexto)
+
+
+def clasificacao(requisicao):
+    return render(requisicao, 'base/clasificacao.html')
--- quiz/urls.py
+++ quiz/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from quiz.base import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('clasificacao', views.clasificacao),
+    path('perguntas/<int:indice>', views.perguntas),
 ]
